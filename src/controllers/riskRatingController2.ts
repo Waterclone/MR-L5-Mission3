6435bb8650e57a55e682bce0e4284498c1309cd5
Jimson Montederamos <Jimson@missionreadyhq.com>
2023-08-17 20:03:48 +1200

    version2

--- src/controllers/riskRatingController2.ts
+++ src/controllers/riskRatingController2.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from "express";
 import { riskRatingCalc } from "../services/riskRatingServices2";
 
-export const getRiskRating2 = ((req: Request, res: Response) => {
+export const getRiskRating2 = async (req: Request, res: Response) => {
     try {
-        const input = req.query;
+        const claim_history: string = req.body.claim_history;
 
-        const output = riskRatingCalc({ claim_history: input.claim_history as string });
+        if (!claim_history || typeof claim_history !== 'string') {
+            return res.status(400).json({ error: 'Invalid claim history data' });
+        }
+
+        const output = await riskRatingCalc(claim_history);
         res.json(output);
     } catch (error) {
         console.error('Error in getRiskRating:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
-});
+};
--- src/services/carValueServices.ts
+++ src/services/carValueServices.ts
@@ -1,14 +1,26 @@
 export const calculateCarValue = (model: string, year: number): number => {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    let carValue = 0;
-
-    for (let i = 0; i < model.length; i++) {
-        const char = model[i].toLowerCase();
-        if (alphabet.includes(char)) {
-            carValue += alphabet.indexOf(char) + 1;
+    try {
+        if (!model || !year || model.trim() === "" || year < 2000 || year > new Date().getFullYear() || typeof year !== 'number' || year < 0 || !Number.isInteger(year)) {
+            throw new Error("Invalid input");
         }
-    }
 
-    carValue = carValue * 100 + year;
-    return carValue;
+        const alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
+        let carValue: number = 0;
+
+        for (let i = 0; i < model.length; i++) {
+            const char = model[i].toLowerCase();
+            if (alphabet.includes(char)) {
+                carValue += alphabet.indexOf(char) + 1;
+            }
+        }
+
+        carValue = carValue * 100 + year;
+        return carValue;
+    } catch (error) {
+        console.error('Error in calculateCarValue:', error);
+        throw error;
+    }
 };
+
+
+
